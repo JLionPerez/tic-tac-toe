0dad536915e04814512964e983387c9503f73878
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-09-19 16:37:55 -0700

    Add players

--- game.js
+++ game.js
@@ -7,14 +7,22 @@ function createBoard () {
     return { board }
 }
 
-function createPlayer () {
-    
+function createPlayer (mark) {
+    return { mark }
 }
+
+const gameboard = createBoard()
+const player1 = createPlayer("X")
+const player2 = createPlayer("O")
+
+console.log({
+        board: gameboard.board,
+        player1: player1.mark,
+        player2: player2.mark
+    })
 
 // factories
 // make Gameboard >
 // make player object
 // make object for controlling the flow of game (game obj)
 // all wrapped in IIFE
-
-console.log(createBoard())
