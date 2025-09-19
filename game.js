93a63cbbe8bef694b1df7c824a580ffce8d713af
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-09-19 16:16:52 -0700

    Add gameboard

--- game.js
+++ game.js
@@ -1,9 +1,20 @@
-const Gameboard = function () {
+function createBoard () {
+    let board = [
+        "","","",
+        "","","",
+        "","",""
+    ]
+    return { board }
+}
 
+function createPlayer () {
+    
 }
 
 // factories
-// make Gameboard
+// make Gameboard >
 // make player object
-// make object for controlling the flow of game
+// make object for controlling the flow of game (game obj)
 // all wrapped in IIFE
+
+console.log(createBoard())
