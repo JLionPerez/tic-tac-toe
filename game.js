699023c30ff315674a6221c97bf33e3577c68f88
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-09-24 15:46:43 -0700

    Understanding IIFEs

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 function createBoard () {
     let board = [
-        "","","",
-        "","","",
-        "","",""
+        ["","",""],
+        ["","",""],
+        ["","",""]
     ]
     return { board }
 }
@@ -15,14 +15,24 @@ const gameboard = createBoard()
 const player1 = createPlayer("X")
 const player2 = createPlayer("O")
 
-console.log({
-        board: gameboard.board,
-        player1: player1.mark,
-        player2: player2.mark
-    })
+// console.log({
+//         board: gameboard.board,
+//         player1: player1.mark,
+//         player2: player2.mark
+//     })
+
+// when players turn
+
+// put mark on given index
+
+
 
 // factories
 // make Gameboard >
 // make player object
 // make object for controlling the flow of game (game obj)
 // all wrapped in IIFE
+
+const Game = (function () {
+    console.log("Hi, I'm the game")
+})();
