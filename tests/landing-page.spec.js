4faebab6a768221559518124d88664083d922f6b
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2026-04-27 18:50:08 -0700

    Add test for reset button + move url to top of file no duplicates

--- tests/landing-page.spec.js
+++ tests/landing-page.spec.js
@@ -1,23 +1,33 @@
 import { test, expect } from '@playwright/test';
 
-test('page has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('https://jlionperez.github.io/tic-tac-toe/');
+})
+
+test('page has title', async ({ page }) => {
     await expect(page).toHaveTitle(/Tic Tac Toe/);
 });
 
-test('welcome player 1 appears first', async ({ page }) => {
-    await page.goto('https://jlionperez.github.io/tic-tac-toe/');
-    await expect(page.getByText('Welcome to Tic Tac Toe!')).toBeVisible();
-    await expect(page.locator('body')).toContainText('Welcome to Tic Tac Toe! Player 1 will be starting this round.');
+test('page has message board', async ({ page }) => {
+    await expect(page.locator('.message-board')).toBeVisible();
 })
 
+test('welcome player 1 appears first', async ({ page }) => {
+    await expect(page.locator('body')).toContainText('Welcome to Tic Tac Toe! Player 1 will be starting this round.');
+});
+
 test('all 9 cells in game board rendered and empty', async ({ page }) => {
-    await page.goto('https://jlionperez.github.io/tic-tac-toe/');
     await expect(page.locator('.gameboard')).toBeVisible();
     
     const cells = page.locator('.cell')
     await expect(cells).toHaveCount(9)
-    for (let i = 0; i < cells.count(); i++) {
+
+    const count  = await cells.count()
+    for (let i = 0; i < count; i++) {
         await expect(cells.nth(i)).toHaveText('');
-    }
+    }   
 });
+
+test('page has reset button', async ({ page }) => {
+    await expect(page.locator('#reset_btn')).toBeVisible();
+})
